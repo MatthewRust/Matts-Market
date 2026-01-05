a2e035f993c9bdc2eb9de7a21a658cc527266c70
Matthew Rust <2888890R@student.gla.ac.uk>
2026-01-05 17:38:28 +0000

    more render fixes 2

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { api } from "@/lib/api";
+import api from "@/lib/api";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
